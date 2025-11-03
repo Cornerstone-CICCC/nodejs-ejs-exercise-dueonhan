1196e34e1b3cc14e8768d254a219e112b0ed1f05
dueon <hdejh79@gmail.com>
2025-11-03 11:53:50 -0800

    I'm done

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,36 @@
 // This is your server file :)
+
+import express, { Request, Response, NextFunction } from "express";
+import path from "path";
+import dotenv from "dotenv";
+dotenv.config();
+import pageRouter from "./routes/page.routes";
+
+// Create server
+const app = express();
+
+// Set view engine to EJS
+app.set("view engine", "EJS");
+app.set("views", path.join(__dirname, "../src/views")); // Tell Express where to look for the EJS pages
+
+// Middleware
+app.use(express.json()); // Allow and parse JSON body
+app.use(express.static(path.join(__dirname, "public"))); // Set the public directory
+app.use(express.urlencoded({ extended: true })); // Allow form submission
+
+// Routes
+app.use("/", pageRouter);
+
+// Fallback / 404
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(404).send("Invalid route!");
+});
+
+// Start server
+const PORT = process.env.PORT;
+if (!PORT) {
+  throw new Error("Missing port!");
+}
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
